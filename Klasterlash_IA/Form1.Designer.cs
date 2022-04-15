23b03b58e0efed9f502e75cae89d8784f1bcae51
Elbek Abdurahimov <abdurahimovelbek111@gmail.com>
2022-04-15 20:45:35 +0500

    Add project files.

---
+++ Klasterlash_IA/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Klasterlash_IA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1228, 393);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(142, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Natija ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(86, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 36);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(41, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "K :";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 441);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(218, 30);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Ajratilgan klasterlar";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 474);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(839, 216);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 749);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Klasterlash_IA/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Klasterlash_IA
+{
+
+    public partial class Form1 : Form
+    {
+        int k = 0;        
+        List<List<int>> groups = new List<List<int>>();
+        double [,]centers;
+        public string path;
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text|*.txt|All|*.*";
+            openFileDialog.Title = "Text faylni tanlang";
+            openFileDialog.InitialDirectory = @"C:\Users\Downloads";
+            openFileDialog.ShowDialog();
+            path = openFileDialog.FileName;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read();
+        }
+        public void Read()
+        {            
+            int j = 1;
+            using (StreamReader reader = new StreamReader(path))
+            {               
+                string ss = reader.ReadLine();
+                int probil = 0, element = 0;
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    if(ss[i]==' ')
+                    {
+                        probil++;
+                    }                   
+                }
+                element = reader.ReadLine().Length - probil;
+                string satr = "";
+                if (element>probil)
+                {
+                    for (int i = 1; i <=element+1; i++)
+                    {
+                        if(i==1)
+                        {
+                            satr += " " + "№";
+                        }
+                        else
+                        {
+                             satr +=" "+ (i-1);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i <=element; i++)
+                    {
+                        satr +=" "+i;
+                    }
+                }                             
+                string[] qatorr = satr.Split(' ');
+                DataTable dataTable = new DataTable();
+                foreach (var ustun in qatorr)
+                {
+                    if(ustun=="")
+                    {
+                        continue;
+                    }
+                    dataTable.Columns.Add(ustun);
+                }
+                string newLine;
+                using(StreamReader reader1=new StreamReader(path))
+                {
+                    while ((newLine = reader1.ReadLine()) != null)
+                    {                       
+                        newLine = j.ToString() + " " + newLine;
+                        j++;
+                        DataRow dataRow = dataTable.NewRow();
+                        string[] values = newLine.Split(' ');
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            if (values[i] == "")
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                dataRow[i] = values[i];
+                            }
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                    reader1.Close();
+                    dataGridView1.DataSource = dataTable;
+                }               
+            }
+        }
+        public void Hisobla()
+        {    
+                Klaster();
+                Markaz();
+        }
+        public double Masofa(int group, int tanlanma)
+        {
+            double s = 0;
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                s += Math.Pow(double.Parse(dataGridView1.Rows[tanlanma].Cells[i].Value.ToString()) - centers[group, i], 2);
+            }
+            return Math.Sqrt(s);
+        }
+        public void Markaz()
+        {
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    centers[i, j] = 0;
+                }
+            }
+            for (int j = 0; j < groups.Count; j++)
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    if (i <dataGridView1.Columns.Count)
+                        centers[groups[j][0], groups[j][1]] += (double.Parse(dataGridView1.Rows[groups[j][0]].Cells[i].Value.ToString()) + centers[groups[j][0], groups[j][1]]) / 2;
+                }
+            }
+        }     
+        public void Klaster()
+        {            
+           groups.Clear();   
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                List<int> te = new List<int>();
+                int x = 0;
+                double mas = 0;
+                for (int j = 0; j < k; j++)
+                {
+                    if (j == 0)
+                    {
+                        x = 0;
+                        mas = Masofa(j, i);
+                    }
+                    else
+                        if (mas > Masofa(j, i))
+                    {
+                        mas = Masofa(j,i);
+                        x = j;
+                    }                   
+                }
+                te.Add(x);
+                te.Add(i);
+                groups.Add(te);               
+            }
+
+        }
+        public void Chiqar()
+        {           
+            DataTable dataTable = new DataTable();
+            string satr = "";
+            for (int i = 1; i <= dataGridView1.Rows.Count; i++)
+            {
+                satr += $"{i}-Indeks" + " ";
+            }
+            string[] X = satr.Split(' ');
+            foreach (var item in X)
+            {
+                dataTable.Columns.Add(item);
+            }
+            List<int> vs = new List<int>();
+            int s = dataGridView1.Rows.Count;
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < groups.Count; j++)
+                {
+                    if (i == groups[j][0])
+                        vs.Add(groups[j][1]);
+                }
+                DataRow dataRow = dataTable.NewRow();
+                int kk = 0;
+                for (int g = 1; g <s; g++)
+                {
+                    if ((g-1) == 0)
+                    {
+                        dataRow[g-1] = i+1;
+                    }
+                    if ((g) <= vs.Count)
+                    {
+                        dataRow[g] = vs[kk];
+                        kk++;
+                    }
+                    else
+                    {
+                        dataRow[g] = null;
+                    }
+                }
+                kk = 0;
+                dataTable.Rows.Add(dataRow);
+                vs.Clear();
+            }
+            dataGridView2.DataSource = dataTable;
+        }
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                MessageBox.Show("K kiritilmadi.");
+            else
+            {
+                k = int.Parse(textBox1.Text);
+                if (k < dataGridView1.Rows.Count)
+                {
+                    centers = new double[k, dataGridView1.Rows.Count];
+                    for (int i = 0; i < k; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                        {
+                            if (j < dataGridView1.Columns.Count)
+                                centers[i, j] = double.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                        }
+                    }
+                    Hisobla();
+                    Chiqar();
+                }
+
+                else
+                    MessageBox.Show("K kichik bo'lsin tanlanmalar sonidan!!!");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
